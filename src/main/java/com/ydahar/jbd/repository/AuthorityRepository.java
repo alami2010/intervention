a389ede2306501af216379030a696e0e875fcd64
Youssef Dahar <youssef.dahar@gmail.com>
2023-07-07 00:07:03 +0200

    Initial version of intervention generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/ydahar/jbd/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.ydahar.jbd.repository;
+
+import com.ydahar.jbd.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/ydahar/jbd/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.ydahar.jbd.service.dto;
